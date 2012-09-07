8812950d716426a186c09b6ea160fdb1bd013f8b
Abhishek Das <das.abhshk@gmail.com>
2012-09-07 18:16:33 +0530

    Added bootstrap-responsive.min.css and docs.css to latest-bootstrap.sh

--- latest/latest-bootstrap.sh
+++ latest/latest-bootstrap.sh
@@ -9,12 +9,18 @@ do
         echo "Downloading Twitter Bootstrap v2.1.0: Default Theme"
         curl -s http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.0/css/bootstrap-combined.min.css > bootstrap.min.css    
         echo "Succesfully Downloaded to bootstrap.min.css"
+        curl -s http://bootswatch.com/assets/css/bootstrap-responsive.min.css > bootstrap-responsive.min.css
+        curl -s http://bootswatch.com/css/docs.css > docs.css
+        echo "Downloaded bootstrap-responsive.min.css & docs.css"
     else
         status=`curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://netdna.bootstrapcdn.com/bootswatch/2.1.0/$theme/bootstrap.min.css`
         if [ $status -eq 200 ]; then
             echo "Downloading Twitter Bootstrap v2.1.0: $theme Theme"
-            curl -s http://netdna.bootstrapcdn.com/bootswatch/2.1.0/$theme/bootstrap.min.css > bootstrap-$theme.min.css
-            echo "Succesfully Downloaded to bootstrap-$theme.min.css"
+            curl -s http://netdna.bootstrapcdn.com/bootswatch/2.1.0/$theme/bootstrap.min.css > $theme.bootstrap.min.css
+            echo "Succesfully Downloaded to $theme.bootstrap.min.css"
+            curl -s http://bootswatch.com/assets/css/bootstrap-responsive.min.css > bootstrap-responsive.min.css
+            curl -s http://bootswatch.com/css/docs.css > docs.css
+            echo "Downloaded bootstrap-responsive.min.css & docs.css"
         else
             echo "Invalid Theme Specified"
         fi
